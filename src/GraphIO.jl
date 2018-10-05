5d3391bebe37f4c4bc3814e27503d14c9ce53b26
Seth Bromberger <github@bromberger.com>
2018-10-05 12:57:56 -0700

    wow.

--- src/GraphIO.jl
+++ src/GraphIO.jl
@@ -7,12 +7,15 @@ import LightGraphs: loadgraph, loadgraphs, savegraph, AbstractGraphFormat
 using EzXML
 using ParserCombinator.Parsers.DOT
 using ParserCombinator.Parsers.GML
+using CodecZlib
 
 export DOTFormat, GEXFFormat, GMLFormat, Graph6Format,
-GraphMLFormat, NETFormat, EdgeListFormat, CDFFormat
+GraphMLFormat, NETFormat, EdgeListFormat, CDFFormat,
+LGCompressedFormat
 # package code goes here
 
 #include("jld.jl")
+include("lgcompressed.jl")
 include("dot.jl")
 include("gexf.jl")
 include("gml.jl")
