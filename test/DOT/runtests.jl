0f6faca2b655daea1f7c6b8fbd90f116d7838951
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-03-24 23:44:59 +0530

    add test for single graph

--- test/DOT/runtests.jl
+++ test/DOT/runtests.jl
@@ -14,22 +14,30 @@ using LightGraphs.Experimental
     read_test(DOTFormat(), dg, "g2", fname)
     read_test_mult(DOTFormat(), Dict{String,AbstractGraph}("g1"=>g, "g2"=>dg), fname)
 	
-	fname = joinpath(testdir, "testdata", "savedgraphs.dot")
-	
+	#tests for multiple graphs
+
+	fname = joinpath(testdir, "testdata", "saved3graphs.dot")
 	#connected graph
 	g1 = SimpleGraph(5,10)	
 	#disconnected graph
 	g2 = SimpleGraph(5,2)
 	#directed graph
 	dg = SimpleDiGraph(5,8)
-
 	GraphDict = Dict("g1" => g1, "g2" => g2, "dg" => dg)
 	write_test(DOTFormat(), GraphDict, fname, remove = false, silent = true)
-	
+
 	#adding this test because currently the Parser returns unordered vertices
 	@test has_isomorph(loadgraph(fname, "g1", DOTFormat()), g1)
 	@test has_isomorph(loadgraph(fname, "g2", DOTFormat()), g2)
 	@test has_isomorph(loadgraph(fname, "dg", DOTFormat()), dg)
 
 	rm(fname)
+
+	#tests for single graph
+
+	fname = joinpath(testdir, "testdata", "saved1graph.dot")
+	write_test(DOTFormat(), g1, "g1", fname, remove = false, silent = true)
+	@test has_isomorph(loadgraph(fname, "g1", DOTFormat()), g1)
+
+	rm(fname)	
 end
